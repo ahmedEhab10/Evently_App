3cf2f08c3c761c76fb52a06e9e9cd449558ac79d
Ahmed Ehab <ahmedhobaa24@gmail.com>
2025-10-09 22:53:26 +0300

    test

--- lib/featuers/main_layout/widgets/person_tap/widgets/person_screen_body.dart
+++ lib/featuers/main_layout/widgets/person_tap/widgets/person_screen_body.dart
@@ -20,7 +20,7 @@ class PersonScreenBody extends StatelessWidget {
           width: double.infinity,
 
           decoration: BoxDecoration(
-            color: ColorsManager.red,
+            color: ColorsManager.blue,
             borderRadius: BorderRadius.only(bottomLeft: Radius.circular(36.r)),
           ),
           child: SafeArea(
